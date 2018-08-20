dfdcf64e35ef9ba6b676f48342e5a4522c1a7e26
oomusou <oomusou@gmail.com>
2018-08-20 21:48:20 +0800

    using statement

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            using (var streamReader = new StreamReader("TestFile.txt"))
+            {
+                var line = streamReader.ReadToEnd();
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
